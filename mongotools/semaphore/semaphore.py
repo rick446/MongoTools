156f80ac35427e0146cbf41b1ab3122e30b77566
Cameron Lane <cameron@synapp.io>
2014-04-15 17:28:35 -0400

    Handle upsert with multiple semaphores better

--- mongotools/semaphore/semaphore.py
+++ mongotools/semaphore/semaphore.py
@@ -7,7 +7,9 @@ class Semaphore(object):
         self._name = collection_name
         self._id = id
         self._counter = counter
-        self._db[self._name].update(dict(_id=id), {'$setOnInsert':{counter:value}}, upsert=True)
+        doc = self._db[self._name].find_and_modify(dict(_id=id), {'$setOnInsert':{counter:value}}, upsert=True, new=True)
+        if counter not in doc:
+            self._db[self._name].update({'_id':id, counter:{'$exists':False}}, {'$set': {counter:value}}, w=1)
 
     def acquire(self):
         doc = self._db[self._name].update({'_id':self._id, self._counter:{'$gt':0}}, {'$inc': {self._counter:-1} }, w=1)
